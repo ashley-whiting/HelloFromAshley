204487081067ddf0c43ef5b5163da0af7207d1b9
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2021-01-26 16:27:29 -0800

    Updated from Jandy

--- src/Prove04/Main.java
+++ src/Prove04/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, my name is Ashley.");
+        System.out.println("Hi, Ashley, it's Jandy");
         
     }
 }
